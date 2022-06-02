baa1edb1696d7fb372ddc7674511cbca1ccf6f8e
Ronald Huidrom <ronhuidrom@gmail.com>
2022-06-02 17:05:36 +0500

    Add files via upload

--- auto/NathansonSolutions.el
+++ auto/NathansonSolutions.el
@@ -5,6 +5,14 @@
                      '(("article" "11pt" "leqno")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "margin=3cm")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
